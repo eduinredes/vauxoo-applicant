7ef4c7667522903771b9bd1b34549a07129cb955
eduintesis <eduinestudio@gmail.com>
2017-09-24 20:48:48 -0400

    py1

--- primes.py
+++ primes.py
@@ -1,16 +1,35 @@
 """
-Your module documentation here
+Module designed for Vauxoo technical test
+
+Classes:
+    PrimeClass: class designed for calculating prime numbers
+
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Class to manipulate prime numbers
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Method that determines if a given number is prime
+
+        Args:
+            num_int (int): number to determine if it is prime
+
+        Returns:
+            True if the num_int parameter is prime, otherwise False
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+
+        if num_int < 2:
+            return False
+
+        prime_flag = True
+        for num in range(2, num_int):
+            if num_int % num == 0:
+                prime_flag = False
+                break
+
+        return prime_flag
